fa02da7f02cc69e46496a3b2aa46a26894668bbd
unknown <yogesh.edekar@gmail.com>
2025-01-04 05:37:53 -0800

    Adding initial logic for sub-command processing

--- cmd/task/cmd/add.go
+++ cmd/task/cmd/add.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -10,7 +11,8 @@ var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Adds a task to task list",
 	Run: func(cmd *cobra.Command, arguments []string) {
-		fmt.Println("Add called")
+		taskName := strings.Join(arguments, " ")
+		fmt.Printf("Added \"%s\" to your list", taskName)
 	},
 }
 
--- cmd/task/cmd/do.go
+++ cmd/task/cmd/do.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -10,27 +11,19 @@ import (
 var doCmd = &cobra.Command{
 	Use:   "do",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("do called")
+		var ids []int
+		for _, arg := range args {
+			taskId, err := strconv.Atoi(arg)
+			if nil != err {
+				fmt.Printf("Error occured while parsing %s\n", arg)
+			}
+			ids = append(ids, taskId)
+		}
+		fmt.Printf("%v", ids)
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(doCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// doCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// doCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
